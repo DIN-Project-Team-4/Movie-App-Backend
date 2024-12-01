920e33e59e688ed72ca7f39b4233c7f5bcfa98b0
Indunil Maheshika Liyanage <156415823+t3inli00@users.noreply.github.com>
2024-12-01 16:53:45 +0200

    Display all the reviews backend

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,11 +1,11 @@
 const {queryDb} = require("../repository/queryDatabase.js")
 
 // Function to add a new review to database
-const addReview = async (movieId, description, rating, reviewedAt, userId) => {
-    const sql = 'insert into "Review"(movie_id,description,rating,reviewed_at,user_id) values ($1, $2, $3, $4, $5) returning *'
+const addReview = async (movieId, description, rating, reviewedAt, movieTitle, moviePosterUrl, userId) => {
+    const sql = 'insert into "Review"(movie_id, description, rating, reviewed_at, movie_title, movie_poster_url, user_id) values ($1, $2, $3, $4, $5, $6, $7) returning *'
     let result
     try {
-        result = await queryDb(sql, [movieId, description, rating, reviewedAt, userId])
+        result = await queryDb(sql, [movieId, description, rating, reviewedAt, movieTitle, moviePosterUrl, userId])
         if (!result.rows.length) {
             throw new Error('Failed to insert review')
         }
@@ -29,17 +29,18 @@ const alreadyReviewed = async (movieID, userId) => {
     return result.rows.length > 0
 }
 
-// Function to get all reviews of a given movie
+// Function to get all reviews 
 const getReviews = async (movieId, viewAllReviews) => {
-    const sql = `select R.review_id,R.movie_id,R.description,R.rating,R.reviewed_at,R.user_id,U.username from "Review" R inner join  
-                "User" U on R.user_id = U.user_id where movie_id = ($1) Order By R.reviewed_at desc ${!viewAllReviews? "fetch first 1 rows only": ""}`
-    console.log(sql)
+    movieId?.trim() !== ""? viewAllReviews : viewAllReviews=true
+    const sql = `select R.review_id, R.movie_id, R.description, R.rating, R.reviewed_at, R.movie_title, R.movie_poster_url, R.user_id, U.username from "Review" R inner join  
+                "User" U on R.user_id = U.user_id ${movieId?.trim() !== ""? "where movie_id = ($1)": ""} Order By R.reviewed_at desc ${viewAllReviews==false? "fetch first 10 rows only": ""}`
     try {
-        const result = await queryDb(sql, [movieId])
+        const result = await queryDb(sql, movieId?.trim() !== ""? [movieId]: "")
         return result.rows
     } catch (error) {
         throw new Error(`Error getting reviews: ${error.message}`);
     }
 }
+
 
 module.exports = {addReview, alreadyReviewed, getReviews}
