15c369d089047a098aed84dd36b3e1ab9e432014
ChanAug1st <ChAugustchanjuan@outlook.com>
2024-12-01 21:56:16 +0200

    Added delete functionality

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,11 +3,12 @@
 const express = require('express');
 const { AuthMiddleware } = require('../middleware/authMiddleware')
 const router = express.Router();
-const {onRegister, onFindOneUser, onFindUsers,onFindOneUserbyEmail} = require('../controllers/userController');
+const {onRegister, onFindOneUser, onFindUsers,onFindOneUserbyEmail,onDeleteUser} = require('../controllers/userController');
 
 router.post(`${process.env.BASE_URI}/users`, onRegister); 
 router.get(`${process.env.BASE_URI}/users/:userId`, AuthMiddleware, onFindOneUser); 
 router.get(`${process.env.BASE_URI}/users`, AuthMiddleware, onFindUsers); 
+router.delete(`${process.env.BASE_URI}/users/:userId`, AuthMiddleware, onDeleteUser);
 //router.post(`${process.env.BASE_URI}/find-user-by-email`, AuthMiddleware, onFindOneUserbyEmail); 
 //router.get(`${process.env.BASE_URI}/users`, AuthMiddleware, onFindOneUserbyEmail); 
 
